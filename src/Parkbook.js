d06081f2798ae8531337df5df5ee4256cd689f6c
Dan Ross <daniellross@gmail.com>
2021-03-15 15:30:35 -0500

    Deleted initial app.js files, added parkbook and tested render

---
+++ src/Parkbook.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+// appication views
+// navbar
+// login
+// register
+// import "./Parkbook.css"
+
+export const Parkbook = () => {
+    return (
+        <>
+            <h1>Welcome to parkbook. </h1>
+        </>
+    )
+}

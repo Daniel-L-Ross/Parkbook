ef32cc766e83ec6b965e96e2fe29a185b9269983
Dan Ross <daniellross@gmail.com>
2021-03-15 16:41:54 -0500

    used json.parse on json string to get address object

--- src/components/parks/Park.js
+++ src/components/parks/Park.js
@@ -3,11 +3,13 @@ import React from "react"
 export const ParkCard = ({ park }) => {
 
 console.log(typeof park.mapped_location.human_address)
+const address = JSON.parse(park.mapped_location.human_address)
+debugger
     return (
         <section className="park">
             <h3 className="park__name">{park.park_name}</h3>
             <p>Park Size: {park.acres} acres</p>
-            <div>Address: {park.mapped_location.human_address}</div>
+            <div>Address: {address.address} {address.city}, {address.state} {address.zip} </div>
         </section>
     )
 }

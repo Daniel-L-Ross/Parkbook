46c66710181b8682e98f98f5e8ed85d912f72ab0
Dan Ross <daniellross@gmail.com>
2021-03-16 10:23:56 -0500

    renamed Park.js to ParkCard. Added button for favorite. Added FavoritesProvider

--- src/components/parks/ParkCard.js
+++ src/components/parks/ParkCard.js
@@ -4,12 +4,15 @@ export const ParkCard = ({ park }) => {
 
     // use JSON.parse as nested address data was returned as a JSON object
 const address = JSON.parse(park.mapped_location.human_address)
-debugger
+
     return (
         <div className="park">
             <h3 className="park__name">{park.park_name}</h3>
             <p>Park Size: {park.acres} acres</p>
             <div>Address: {address.address} {address.city}, {address.state} {address.zip} </div>
+            <div className="park__buttons">
+                <button>Favorite</button>
+            </div>
         </div>
     )
 }
--- src/components/parks/ParkList.js
+++ src/components/parks/ParkList.js
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect } from "react"
 import { ParkContext } from "./ParkProvider"
-import { ParkCard } from "./Park"
+import { ParkCard } from "./ParkCard"
 import "./Park.css"
 
 export const ParkList = () => {

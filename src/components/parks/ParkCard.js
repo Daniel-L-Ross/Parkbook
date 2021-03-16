95292bc03b4ed6f1cf3b63e1790631741a20c103
Dan Ross <daniellross@gmail.com>
2021-03-16 13:29:50 -0500

    Added logic so favorite button does not render if the park has been added already

--- src/components/parks/ParkCard.js
+++ src/components/parks/ParkCard.js
@@ -4,10 +4,10 @@ import { FavoriteContext } from "../favorites/FavoritesProvider"
 
 export const ParkCard = ({ park }) => {
     const { userFavorites, getUserFavorites, addFavorite } = useContext(FavoriteContext)
-    
+
     // use JSON.parse as nested address data was returned as a JSON object
     const address = JSON.parse(park.mapped_location.human_address)
-    
+
     const currentUserId = parseInt(sessionStorage.parkbook_user_id)
 
     const handleClickFavorite = event => {
@@ -16,18 +16,25 @@ export const ParkCard = ({ park }) => {
             userId: currentUserId
         }
         addFavorite(newFavorite)
-        .then(getUserFavorites)
+            .then(getUserFavorites)
     }
 
-    
+    let favorited = false
 
+    const favoriteCheck = userFavorites.find(favorite => favorite.parkId === park.id)
+    
+    if (favoriteCheck !== undefined) {
+        favorited = true
+    }
+    
     return (
         <div className="park">
             <h3 className="park__name">{park.park_name}</h3>
             <p>Park Size: {park.acres} acres</p>
             <div>Address: {address.address} {address.city}, {address.state} {address.zip} </div>
             <div className="park__buttons">
-                <button onClick={handleClickFavorite}>Favorite</button>
+                {favorited ? "" : <button onClick={handleClickFavorite}>Favorite</button>}
+
             </div>
         </div>
     )

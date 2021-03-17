53d129925803982759cbae0239dfd427b1b39d04
Dan Ross <daniellross@gmail.com>
2021-03-17 12:51:05 -0500

    working on rendering features to select

--- src/components/parks/ParkSearch.js
+++ src/components/parks/ParkSearch.js
@@ -1,18 +1,36 @@
-import React, { useContext } from "react"
+import React, { useContext, useEffect, useState } from "react"
 import { ParkContext } from "./ParkProvider"
 import "./Park.css"
 
 export const ParkSearch = () => {
-    const { setSearchTerms } = useContext(ParkContext)
+    const { setSearchTerms, parks, getParks } = useContext(ParkContext)
+    const [features, setFeatures] = useState([])
 
     let searchArray = []
 
+    useEffect(() => {
+        const parkFeatures = () => {
+            let featureArray = []
+            
+            Object.keys(parks[0]).map(feature => {
+                if (parks[0][feature] === "Yes" || "No") {
+                    const prettyFeature = feature.replace(/_/g, ' ')
+                    featureArray.push(prettyFeature)
+                }
+            })
+            return featureArray
+        }
+        getParks()
+        .then(setFeatures(parkFeatures))
+    }, [])
+        
     return (
         <>
             Park Search:
             <label htmlFor="features">Features:</label>
             <select>
                 <option value="0">Select a feature</option>
+                {features.map(feature => <option value={feature} key={feature}>{feature}</option>)}
             </select>
         </>
     )

73a1685521cb34cd2a25f068decb7471f25d33df
Dan Ross <daniellross@gmail.com>
2021-03-18 10:37:48 -0500

    Added review file structure and started reviewForm

---
+++ src/components/reviews/ReviewForm.js
@@ -0,0 +1,16 @@
+import React, { useContext, useEffect, useState } from "react"
+import { useHistory, useParams } from "react-router-dom"
+// import { ReviewContext } from "./ReviewProvider"
+import "./Review.css"
+
+export const ReviewForm = () => {
+
+
+    const reviewId = useParams()
+
+    return (
+        <form className="reviewForm">
+            <h2 className="reviewForm__title">{reviewId ? "Edit Review" : "New Review"}</h2>
+        </form>
+    )
+}

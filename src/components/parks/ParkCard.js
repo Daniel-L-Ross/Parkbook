99221dc6902a972fbf6c8277c7eb1d68f44d68c5
Dan Ross <daniellross@gmail.com>
2021-03-26 11:37:06 -0500

    found a bug that cause reviews to post to the wrong park. fixed with useEffect on reviewForm to update review state

--- src/components/parks/ParkCard.js
+++ src/components/parks/ParkCard.js
@@ -6,7 +6,7 @@ import { ReviewContext } from "../reviews/ReviewProvider"
 
 export const ParkCard = ({ park }) => {
     const { userFavorites, getUserFavorites, addFavorite, deleteFavorite } = useContext(FavoriteContext)
-    const { setDisplayReviews, setReviewPark } = useContext(ReviewContext)
+    const { setReviewPark } = useContext(ReviewContext)
 
     // Nested address data was returned as a JSON object
     const address = JSON.parse(park.mapped_location.human_address)
@@ -60,7 +60,6 @@ export const ParkCard = ({ park }) => {
 
     const handleReviewsLink = () => {
         setReviewPark(park)
-        setDisplayReviews(true)
     }
 
     // controls state variable to display or hide park details info

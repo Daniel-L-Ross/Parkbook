cec5597813f67f245e450eecdb6b979b8194f699
Dan Ross <daniellross@gmail.com>
2021-04-01 13:21:34 -0500

    copied and udpated functions for GET, ADD, DELETE

---
+++ src/components/hidden/HiddenProvider.js
@@ -0,0 +1,42 @@
+import React, { useState, createContext } from "react"
+import { userStorageKey } from "../auth/authSettings"
+
+export const HiddenContext = createContext()
+
+export const HiddenProvider = (props) => {
+    const [userHidden, setUserHidden] = useState([])
+
+    const getUserHidden = () => {
+        return fetch(`http://localhost:8088/hidden/?userId=${sessionStorage.getItem(userStorageKey)}&_expand=park`)
+            .then(res => res.json())
+            .then((hidden) => {
+            if (sessionStorage.getItem(userStorageKey)) {
+                setUserHidden(hidden)
+            } else {
+                setUserHidden([])
+            }
+            })
+    }
+
+    const addHidden = newHidden => {
+        return fetch("http://localhost:8088/hidden", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(newHidden)
+        })
+    }
+
+    const deletehidden = hiddenId => {
+        return fetch(`http://localhost:8088/favorites/${hiddenId}`, {
+            method: "DELETE"
+        })
+    }
+
+    return (
+        <HiddenContext.Provider value={{
+            userHidden, getUserHidden, addHidden, deletehidden
+        }}
+    )
+}
